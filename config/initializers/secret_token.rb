6f3a927c179842f19954e416ec8f018c1c03c9d1
PaoMar <paomarcasadiego@gmail.com>
2014-02-27 17:28:55 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Payme::Application.config.secret_key_base = 'dc9d3f35315b684f3466537b1c354ffe00fb1314f475da630b29225d88b8fd3cd7063470f00c9ba5f6367315422bfdc76084709dfbdd289ea219dc401cc471ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Payme::Application.config.session_store :cookie_store, key: '_payme_session'
